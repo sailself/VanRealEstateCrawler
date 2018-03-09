cf2ab0fa81bef583e9c03992a0dda97019c5b99c
Kaifan Zhu <kz@mk3.ca>
2018-03-08 23:13:06 -0800

    New spring project init

---
+++ src/main/java/com/sailself/VanRealEstateCrawler/VanRealEstateCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.sailself.VanRealEstateCrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanRealEstateCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanRealEstateCrawlerApplication.class, args);
+	}
+}
